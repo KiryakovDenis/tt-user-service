0c3e40d7ca5881c0969b892fd8865d509138ea8e
Kiryakov Denis <KiryakovDenis@gmail.com>
2025-03-31 20:58:08 +0500

    Initial commit

---
+++ src/main/java/ru/kdv/study/TTUserApplication.java
@@ -0,0 +1,11 @@
+package ru.kdv.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TTUserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TTUserApplication.class, args);
+    }
+}
